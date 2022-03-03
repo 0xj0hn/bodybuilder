224315b13c0c914c8cc0b2e5aafc28bb3bbf4040
John <johnsec@yahoo.com>
2022-03-03 21:33:08 +0330

    update ScheduleWidget

--- lib/utils/widgets.dart
+++ lib/utils/widgets.dart
@@ -1,4 +1,6 @@
+import 'package:bodybuilder/utils/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CustomedTextField extends StatelessWidget {
   @override
@@ -61,7 +63,7 @@ class CustomedButton extends StatelessWidget {
   }
 }
 
-class Schedule extends StatelessWidget {
+class ScheduleWidget extends StatelessWidget {
   @override
   Function()? onPressed;
   String? scheduleName;
@@ -69,7 +71,7 @@ class Schedule extends StatelessWidget {
   String? weeksCount;
   String? coachName;
   String? type;
-  Schedule(
+  ScheduleWidget(
       {this.onPressed,
       this.scheduleName,
       this.startedDate,
@@ -95,25 +97,62 @@ class Schedule extends StatelessWidget {
 تاریخ شروع: $startedDate
 نوع: $type
 """;
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Material(
+        //color: Color(0xffcfd0da),
+        //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        child: Card(
+          color: Color(0xffeaf1ff),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          child: InkWell(
+            focusColor: Color(0xffcfd0da),
+            borderRadius: BorderRadius.circular(30),
+            onTap: onPressed,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text("$text"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DaysWidget extends StatelessWidget {
+  String? daytitle;
+  String? timeAverage;
+  String? activityNumbers;
+  DaysWidget({this.activityNumbers, this.daytitle, this.timeAverage});
+
+  @override
+  Widget build(BuildContext context) {
     return Material(
-      //color: Color(0xffcfd0da),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
       child: Card(
-        color: Color(0xffeaf1ff),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         child: InkWell(
-          focusColor: Color(0xffcfd0da),
-          borderRadius: BorderRadius.circular(30),
-          onTap: onPressed,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Text("$text"),
-              )
-            ],
+          borderRadius: BorderRadius.circular(20),
+          onTap: () {},
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "روز ۱",
+                  style: TextStyleX.boldStyle,
+                ),
+                Text("تعداد حرکات: 5")
+              ],
+            ),
           ),
         ),
       ),
