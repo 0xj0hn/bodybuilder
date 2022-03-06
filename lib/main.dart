b38a4c2cffe8b89c2d8cea3c8498073ab7b63101
John <johnsec@yahoo.com>
2022-03-06 16:36:29 +0330

    changed transition animation that is awesome :)

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      defaultTransition: Transition.topLevel,
       home: Hive.box("auth").get("is_loggined") ? DashboardPage() : Login(),
       locale: Locale('fa', 'IR'),
       theme: ThemeX.lightTheme,
